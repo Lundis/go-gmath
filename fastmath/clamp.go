72536851eb97224aefecdac41a6e3b2ff9f6be92
lundis <lundis.bah@gmail.com>
2025-09-18 21:06:42 +0300

    Added ClampD

--- fastmath/clamp.go
+++ fastmath/clamp.go
@@ -6,3 +6,6 @@ func Clamp(x, lower, upper float32) float32 {
 func ClampI(x, lower, upper int32) int32 {
 	return max(lower, min(upper, x))
 }
+func ClampD(x, lower, upper float64) float64 {
+	return max(lower, min(upper, x))
+}
