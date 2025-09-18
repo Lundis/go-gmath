1a687ae718d00b9faaa7aa7ebc73f39b3f195f92
lundis <lundis.bah@gmail.com>
2025-09-18 07:53:37 +0300

    Added fastmath.Clamp and fastmath.ClampI

---
+++ fastmath/clamp.go
@@ -0,0 +1,8 @@
+package fastmath
+
+func Clamp(x, lower, upper float32) float32 {
+	return max(lower, min(upper, x))
+}
+func ClampI(x, lower, upper int32) int32 {
+	return max(lower, min(upper, x))
+}
